39fd056e922c54d25925cf266763202cf9ebc20e
catherine244 <ccayreen24@gmail.com>
2019-12-02 13:00:24 +0300

    added timeAgo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
-
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 
 @NgModule({
@@ -18,7 +17,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     QuoteDetailComponent,
     DateCountPipe,
     QuoteFormComponent,
-
+    TimeAgoPipe
  
   ],
   imports: [
